0ff374886707fbe9610faf20c33a76c63406ab7d
kora1348 <amelianekor@gmail.com>
2022-10-22 00:55:20 +0200

    menu simple

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HTML_n_CSSComponent } from './views/category/html-n-css.component';
+import { JavascriptComponent } from './views/category/javascript.component';
+import { PHP_n_MySQLComponent } from './views/category/php-n-mysql.component';
+import { DashboardComponent } from './views/dashboard.component';
 
 const routes: Routes = [
   
-  {
-    path: '',
-    loadChildren: () => import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule),
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  // },
+
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'html-n-css', component: HTML_n_CSSComponent },
+  { path: 'javascript', component: JavascriptComponent },
+  { path: 'php-n-mysql', component: PHP_n_MySQLComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './views/dashboard.component';
+import { HTML_n_CSSComponent } from './views/category/html-n-css.component';
+import { JavascriptComponent } from './views/category/javascript.component';
+import { PHP_n_MySQLComponent } from './views/category/php-n-mysql.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent],
+  imports: [CommonModule, BrowserModule, FormsModule,  AppRoutingModule],
+  declarations: [AppComponent, DashboardComponent,  HTML_n_CSSComponent, JavascriptComponent, PHP_n_MySQLComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
