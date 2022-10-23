42f1fac76dc9a7dbf80269f52d13276c82abba3c
kora1348 <amelianekor@gmail.com>
2022-10-23 23:51:22 +0200

    toto

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,67 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuSidebar } from './types/menuSidebar';
 
-
 @Component({
-selector: 'app-dashboard',
-templateUrl: './dashboard.component.html',
-styleUrls: ['./dashboard.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  activeItem: any;
 
-    openSidebar: boolean = true;
+  openSidebar: boolean = true;
 
-    menuSidebar: MenuSidebar[] = [
-       {
-        link_name: 'Category',
-        link: null,
-        icon: 'bx bx-collection',
-        active: false,
-        sub_menu: [
-          {
-            link_name: 'Portfolio',
-            link: '/category/portfolio',
-          }, 
-          {
-            link_name: 'Contact',
-            link: '/category/contact',
-          }, 
-        ]
-      },
-      {
-        link_name: 'Markets',
-        link: null,
-        icon: 'bx bx-collection',
-        active: false,
-        sub_menu: [
-          {
-            link_name: 'Indice',
-            link: '/markets/indice',
-          }, 
-        ]
-      },
-    ]
-  
-    constructor() { }
-  
-    ngOnInit() {
-  
+  menuSidebar: MenuSidebar[] = [
+    {
+      link_name: 'Category',
+      link: null,
+      icon: 'bx bx-collection',
+      active: false,
+      sub_menu: [
+        {
+          link_name: 'Portfolio',
+          link: '/category/portfolio',
+        },
+        {
+          link_name: 'Contact',
+          link: '/category/contact',
+        },
+      ],
+    },
+    {
+      link_name: 'Markets',
+      link: null,
+      icon: 'bx bx-collection',
+      active: false,
+      sub_menu: [
+        {
+          link_name: 'Indice',
+          link: '/markets/indice',
+        },
+      ],
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  showSubmenu(item: any) {
+    if (item.link_name == this.activeItem) {
+      this.activeItem = undefined;
+    } else {
+      this.activeItem = item.link_name;
     }
-  
-    showSubmenu(itemEl: HTMLElement) {
-      itemEl.classList.toggle('showMenu');
-    }
-
-    selectMenu(parentMenu: { link_name: string }) : void {
-
-      this.menuSidebar.forEach(menu => {
-      if (menu.link_name !== parentMenu.link_name) {
-      menu.active = false;
-      } else {
-      menu.active = !menu.active;
-      }
-      })
-      
-      }
   }
-  
+
+  selectMenu(parentMenu: { link_name: string }): void {
+    this.menuSidebar.forEach((menu) => {
+      if (menu.link_name !== parentMenu.link_name) {
+        menu.active = false;
+      } else {
+        menu.active = !menu.active;
+      }
+    });
+  }
+}
